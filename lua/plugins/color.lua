81daa69cbbe8e18768772b2734210b7333fca500
hill212063 <hill_sup@hotmail.com>
2024-10-11 21:34:55 +0700

    Add more config for onedarkpro

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -5,7 +5,11 @@ return {
   config = function()
     require("onedarkpro").setup({
       options = {
-        transparency = true,
+        cursorline = false, -- Use cursorline highlighting?
+        transparency = true, -- Use a transparent background?
+        terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+        lualine_transparency = true, -- Center bar transparency?
+        highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
       },
     })
     vim.cmd("colorscheme onedark")
