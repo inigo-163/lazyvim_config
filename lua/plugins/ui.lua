6d20dbc8504c5890145e9927d180bd3d6d154212
hill212063 <hill_sup@hotmail.com>
2024-10-11 22:29:32 +0700

    Change lualine speratot to empty

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,8 +1,9 @@
 return {
-  "nvimdev/dashboard-nvim",
-  event = "VimEnter",
-  opts = function(_, opts)
-    local logo = [[
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
  ▄█  ███▄▄▄▄    ▄█     ▄██████▄   ▄██████▄  
 ███  ███▀▀▀██▄ ███    ███    ███ ███    ███ 
 ███▌ ███   ███ ███▌   ███    █▀  ███    ███ 
@@ -13,7 +14,20 @@ return {
 █▀    ▀█   █▀  █▀     ████████▀   ▀██████▀  
                                             
   ]]
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
-    opts.config.header = vim.split(logo, "\n")
-  end,
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function()
+      local opts = {
+        options = {
+          section_separators = { left = " ", right = " " },
+          component_separators = { left = " ", right = " " },
+        },
+      }
+      return opts
+    end,
+  },
 }
