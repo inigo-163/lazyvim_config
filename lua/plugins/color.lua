c89c21996329fb3a4473ab4caaa1364966262d49
hill212063 <hill_sup@hotmail.com>
2024-11-17 21:21:08 +0700

    Change color to white theme

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,29 +1,13 @@
 return {
-  "2nthony/vitesse.nvim",
-  dependencies = {
-    "tjdevries/colorbuddy.nvim",
-  },
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require("vitesse").setup({
-      comment_italics = true,
-      transparent_background = true,
-      transparent_float_background = true, -- aka pum(popup menu) background
-      reverse_visual = false,
-      dim_nc = false,
-      cmp_cmdline_disable_search_highlight_group = false, -- disable search highlight group for cmp item
-      -- if `transparent_float_background` false, make telescope border color same as float background
-      telescope_border_follow_float_background = false,
-      -- similar to above, but for lspsaga
-      lspsaga_border_follow_float_background = false,
-      -- diagnostic virtual text background, like error lens
-      diagnostic_virtual_text_background = false,
-
-      -- override the `lua/vitesse/palette.lua`, go to file see fields
-      colors = {},
-      themes = {},
+    require("github-theme").setup({
+      -- ...
     })
-    vim.opt.winblend = 0
-    vim.opt.pumblend = 0
-    vim.cmd("colorscheme vitesse")
+
+    vim.cmd("colorscheme github_light")
   end,
 }
