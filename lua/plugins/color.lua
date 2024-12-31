de107f766e80978525c59a46e781adf720ebe8d2
hill212063 <hill_sup@hotmail.com>
2024-12-31 17:28:47 +0700

    Update and change to clear theme according to match swaywm

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,8 +1,9 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
+  "gnmearacaun/zephyr-clear.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.cmd([[colorscheme oxocarbon]])
+    -- Default options
+    vim.cmd([[colorscheme zephyr_clear]])
   end,
 }
