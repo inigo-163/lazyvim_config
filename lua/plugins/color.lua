23899bb5fccc201a947b944364e85bd4591eb3e4
hill212063 <hill_sup@hotmail.com>
2025-01-08 21:12:32 +0700

    Change color to night owl

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,9 +1,16 @@
 return {
-  "gnmearacaun/zephyr-clear.nvim",
+  "oxfist/night-owl.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    -- Default options
-    vim.cmd([[colorscheme zephyr_clear]])
+    -- load the colorscheme here
+    require("night-owl").setup({
+      bold = true,
+      italics = true,
+      underline = true,
+      undercurl = true,
+      transparent_background = true,
+    })
+    vim.cmd.colorscheme("night-owl")
   end,
 }
