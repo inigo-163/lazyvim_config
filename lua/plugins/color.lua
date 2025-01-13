e4b75b5dd807455bf9420605202fe0110aea8eea
hill212063 <hill_sup@hotmail.com>
2025-01-13 21:56:19 +0700

    Change color to dark flat

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,16 +1,17 @@
 return {
-  "oxfist/night-owl.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  "sekke276/dark_flat.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
   config = function()
-    -- load the colorscheme here
-    require("night-owl").setup({
-      bold = true,
+    require("dark_flat").setup({
+      transparent = true,
+      colors = {},
+      themes = function(colors)
+        return {}
+      end,
       italics = true,
-      underline = true,
-      undercurl = true,
-      transparent_background = true,
     })
-    vim.cmd.colorscheme("night-owl")
+    vim.cmd("colorscheme dark_flat")
   end,
 }
