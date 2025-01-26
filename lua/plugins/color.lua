c6e463bae366d93ea36a2200dcb068b0f940384d
hill212063 <hill_sup@hotmail.com>
2025-01-26 20:20:14 +0700

    Change highlight color to not be too dimmed

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -6,12 +6,24 @@ return {
   config = function()
     require("dark_flat").setup({
       transparent = true,
-      colors = {},
+      colors = {
+        gray = "#808080",
+        white = "#FFFFFF",
+        smoke = "#848884",
+      },
       themes = function(colors)
-        return {}
+        return {
+          CursorLineNr = { fg = colors.white },
+          LineNr = { fg = colors.gray },
+          Visual = { bg = colors.smoke },
+        }
       end,
       italics = true,
     })
     vim.cmd("colorscheme dark_flat")
+    -- Set neotree and noice color to support transparent
+    vim.cmd("highlight! NeoTreeDotfile guifg=#808080")
+    vim.cmd("highlight! NeoTreeMessage guifg=#808080")
+    vim.cmd("highlight! NoiceLspProgressTitle guifg=#808080")
   end,
 }
