99e8baa34a0df548049cbcf412d7f796a5ba41ec
hill212063 <hill_sup@hotmail.com>
2025-02-09 18:31:46 +0700

    Change color to vague and update lazy

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,26 +1,13 @@
 return {
-  "sekke276/dark_flat.nvim",
+  "vague2k/vague.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-    require("dark_flat").setup({
-      transparent = true,
-      colors = {
-        gray = "#808080",
-        white = "#FFFFFF",
-        custom_gray = "#606060",
-      },
-      themes = function(colors)
-        return {
-          CursorLineNr = { fg = colors.white },
-          LineNr = { fg = colors.gray },
-          Visual = { bg = colors.custom_gray },
-        }
-      end,
-      italics = true,
+    require("vague").setup({
+      transparent = true, -- optional configuration here
     })
-    vim.cmd("colorscheme dark_flat")
+    vim.cmd([[colorscheme vague]])
     -- Set neotree and noice color to support transparent
     vim.cmd("highlight! NeoTreeDotfile guifg=#808080")
     vim.cmd("highlight! NeoTreeMessage guifg=#808080")
