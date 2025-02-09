dafe9bd0b003f6d785440d6f41ce7cf2b543b1be
hill212063 <hill_sup@hotmail.com>
2025-02-09 18:44:43 +0700

    Change color to moonfly

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,13 +1,11 @@
 return {
-  "vague2k/vague.nvim",
+  "bluz71/vim-moonfly-colors",
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-    require("vague").setup({
-      transparent = true, -- optional configuration here
-    })
-    vim.cmd([[colorscheme vague]])
+    vim.g.moonflyTransparent = true
+    vim.cmd([[colorscheme  moonfly]])
     -- Set neotree and noice color to support transparent
     vim.cmd("highlight! NeoTreeDotfile guifg=#808080")
     vim.cmd("highlight! NeoTreeMessage guifg=#808080")
