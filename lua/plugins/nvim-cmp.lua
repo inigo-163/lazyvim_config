5b3609e08bfa6bed6f3ea8a8d1587c42fbd9b8cf
inigo-163 <hill_sup@hotmail.com>
2025-03-30 20:18:34 +0700

    Fix Tab jumping btween placeholder

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -26,12 +26,11 @@ return {
             fallback()
           end
         end),
-
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.locally_jumpable(1) then
-            luasnip.jump(1)
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
           else
             fallback()
           end
@@ -40,13 +39,14 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
+          elseif luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
           end
         end, { "i", "s" }),
       }),
+
       sources = cmp.config.sources({
         { name = "lazydev" },
         { name = "nvim_lsp" },
@@ -54,6 +54,12 @@ return {
       }, {
         { name = "buffer" },
       }),
+
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body) -- Ensure snippets expand correctly
+        end,
+      },
     }
   end,
 }
