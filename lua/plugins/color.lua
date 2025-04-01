697fed1fdeb8a9254c52d4f6f59fe1c047bedda2
inigo163 <hill_sup@hotmail.com>
2025-04-01 20:29:59 +0700

    Update color to be more clear on highlight

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -6,9 +6,10 @@ return {
   config = function()
     vim.g.moonflyTransparent = true
     vim.cmd([[colorscheme  moonfly]])
-    -- Set neotree and noice color to support transparent
+    -- Set neotree, noice and highlight color to support transparent
     vim.cmd("highlight! NeoTreeDotfile guifg=#808080")
     vim.cmd("highlight! NeoTreeMessage guifg=#808080")
     vim.cmd("highlight! NoiceLspProgressTitle guifg=#808080")
+    vim.cmd("highlight Visual guibg=#3e4451  guifg=#ffffff")
   end,
 }
